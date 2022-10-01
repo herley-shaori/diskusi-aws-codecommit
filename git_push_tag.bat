298d09c19c3458d49629fa1f845e16b87d10e77e
herley-shaori <herley@sci.ui.ac.id>
2022-10-01 21:49:30 +0700

    done v3

--- git_push_tag.bat
+++ git_push_tag.bat
@@ -1,4 +1,4 @@
 git remote add ea ssh://git-codecommit.ap-southeast-1.amazonaws.com/v1/repos/demo-codecommit-dev &^ 
-git tag v2 19c7effdbb85655eed51a24778e681621dc4e8f9 &^
-git push ea v2 &^
+git tag v3 19c7effdbb85655eed51a24778e681621dc4e8f9 &^
+git push ea v3 &^
 git remote remove ea
