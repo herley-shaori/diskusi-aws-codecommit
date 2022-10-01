6342bc88a77ae4af3bed23e52458b2095ddf750e
herley-shaori <herley@sci.ui.ac.id>
2022-10-01 17:12:48 +0700

    renamed yaml to yml

--- push_to_ea.bat
+++ push_to_ea.bat
@@ -1,5 +1,5 @@
 git add . & ^
-git commit -m 'pesan ke ea' & ^
+git commit -m "renamed yaml to yml" & ^
 git remote add ea ssh://git-codecommit.ap-southeast-1.amazonaws.com/v1/repos/demo-codecommit-dev & ^
 git push -u ea main & ^
 git remote remove ea
